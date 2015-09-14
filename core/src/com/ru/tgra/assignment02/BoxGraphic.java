e1eadbbadca28a38d8b9b4ea711442302c48f76c
SoffiaI <soffia.ingibjargar@gmail.com>
2015-09-14 17:58:51 +0000

    fuck this

---
+++ core/src/com/ru/tgra/assignment02/BoxGraphic.java
@@ -0,0 +1,13 @@
+package com.ru.tgra.assignment02;
+
+import java.util.ArrayList;
+
+public class BoxGraphic {
+
+	ModelMatrix orientation;
+	ArrayList<Box> boxes;
+	
+	public BoxGraphic() {
+		
+	}
+}
